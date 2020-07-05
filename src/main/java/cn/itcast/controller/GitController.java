1c849542d7d72ccd51a69ae5e99c2b346083f6ca
sublun <sublun@126.com>
2020-07-05 17:57:40 +0800

    ls

--- src/main/java/cn/itcast/controller/GitController.java
+++ src/main/java/cn/itcast/controller/GitController.java
@@ -8,6 +8,6 @@ package cn.itcast.controller;
 public class GitController {
     public static void main(String[] args) {
 
-        System.out.println("aaa");
+        System.out.println("aaa 李四");
     }
 }
